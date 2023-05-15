5e4b7f3cce2e05424c4633c8f5074f1015a1c109
SUNLIFAN <lifansun1412@gmail.com>
2023-05-15 20:01:04 +0800

    finish training

--- common/arguments.py
+++ common/arguments.py
@@ -17,13 +17,13 @@ def get_args():
     parser.add_argument("--episode-len", type=int, default=200, help="maximum episode length")
     parser.add_argument("--max_time_steps", type=int, default=1000000, help="number of time steps")
 
-    parser.add_argument("--epsilon", type=float, default=1, help="epsilon greedy")
-    parser.add_argument("--min_epsilon", type=float, default=0.1)
-    parser.add_argument("--epsilon_anneal_time", type=int, default=100000)
+    parser.add_argument("--epsilon", type=float, default=0.8, help="epsilon greedy")
+    parser.add_argument("--min_epsilon", type=float, default=0.01)
+    parser.add_argument("--epsilon_anneal_time", type=int, default=500000)
 
     parser.add_argument("--lr_q", type=float, default=1e-3, help="learning rate of q network")
     parser.add_argument("--gamma", type=float, default=0.99, help="discount factor")
-    parser.add_argument("--buffer_size", type=int, default=int(2000), help="number of transitions can be stored in buffer")
+    parser.add_argument("--buffer_size", type=int, default=int(10000), help="number of transitions can be stored in buffer")
     parser.add_argument("--batch_size", type=int, default=32, help="number of episodes to optimize at the same time")
     parser.add_argument("--target_update_cycle", type=int, default=200)
 
@@ -45,7 +45,7 @@ def get_args():
     parser.add_argument("--device", type=str, default='0', help="which GPU is used")
 
     # Parameters that are only used for policy gradient algorithms
-    parser.add_argument("--max_episodes", type=int, default=5000, help="number of training episodes, only for policy-based methods")        # original: 2000
+    parser.add_argument("--max_episodes", type=int, default=10000, help="number of training episodes, only for policy-based methods")        # original: 2000
     parser.add_argument("--lr_actor", type=float, default=1e-4, help="learning rate of policy")
     parser.add_argument("--lr_critic", type=float, default=1e-3, help="learning rate of critic")
     parser.add_argument("--tau", type=float, default=0.01)
